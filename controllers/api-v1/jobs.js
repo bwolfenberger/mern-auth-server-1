fd6592b3f820007e55b83267fd9b778d84892f0e
erikwinkel <erikpwinkel@gmail.com>
2021-07-15 10:13:40 -0700

    add create and delete routes to jobs

--- controllers/api-v1/jobs.js
+++ controllers/api-v1/jobs.js
@@ -5,8 +5,9 @@ const authLockedRoute = require('./authLockedRoute')
 
 // GET - /jobs - user id and jobId in body
 // probably won't use this because jobs are embedded docs
-router.get('/', authLockedRoute, async (req,res) => {
-    res.json(res.locals.user.jobs[0])
+router.get('/', authLockedRoute, (req,res) => {
+    let i = res.locals.user.jobs.findIndex(job => job._id == req.body.jobId)
+    res.json({ job: res.locals.user.jobs[i] })
 })
 
 //POST - /jobs - new job in body, jwt token in auth headers
@@ -15,26 +16,38 @@ router.post('/', authLockedRoute, async (req,res) => {
         res.locals.user.jobs.push(req.body.job)
         console.log(res.locals.user.jobs)
         await res.locals.user.save()
-        res.json({ msg: 'job successfully created' })
+        res.json({ msg: `job ${req.body.jobId} successfully created` })
     } catch(err) {
         console.log(err)
         res.status(500).json({ msg: 'internal server error '})
     }
 })
 
+// PUT - /jobs - update job. job in body, token in headers
+// might need to change so I don't overwrite mongoose timestamps.
 router.put('/', authLockedRoute, async (req,res) => {
     try {
         // TODO: maybe fix strict equality check? 
         let i = res.locals.user.jobs.findIndex(job => job._id == req.body.job._id)
         res.locals.user.jobs[i] = req.body.job
         await res.locals.user.save()
-        res.json({msg: 'job successfully updated'})
-
+        res.json({msg: `job ${req.body.jobId} successfully updated`})
     } catch(err){
         console.log(err)
         res.status(500).json({ msg: 'internal server error' })
     }
 })
 
+//DELETE - /jobs - delete a job. jobId in body, token in headers
+router.delete('/', authLockedRoute, async (req,res) => {
+    try {
+        res.locals.user.jobs.id(req.body.jobId).remove()
+        res.locals.user.save()
+        res.json({msg: `job ${req.body.jobId} successfully deleted`})
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({ msg: 'internal server error'})
+    }
+})
 
 module.exports = router
